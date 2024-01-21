7f6dc63eb274c9364ba3caf9016490577235974b
Sherif-Mansour <135798032+Sherif-Mansour@users.noreply.github.com>
2024-01-20 21:27:33 -0700

    Complete Week 2 Demo

---
+++ app/week-2/page.js
@@ -0,0 +1,18 @@
+import Heading from './heading.js';
+
+export default function Page() {
+    let a = 5;
+    let b = 10;
+    let c = a + b;
+
+    return (
+        <>
+            <main>
+                <Heading />
+                <p>The sum of {a} and {b} is {c}</p>
+                <p>The sum of {a} and {b} is {a + b}</p>
+            </main>
+        </>
+        
+    )
+  }
