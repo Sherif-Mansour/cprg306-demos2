aae5c99c7d04012a4b8b97c225606f5ff9352547
Sherif-Mansour <135798032+Sherif-Mansour@users.noreply.github.com>
2024-02-07 12:16:26 -0700

    Week-5 Demo

---
+++ app/week-5/page.js
@@ -0,0 +1,8 @@
+export default function Page() {
+    const dogs = [
+        { 
+            name: "Winky", 
+            description: "A winky dog",
+            imageUrl: https://images.pexels.com/photos/2023384/pexels-photo-2023384.jpeg}
+    
+}
