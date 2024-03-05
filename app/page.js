67d505c8c011c04e6a6978ea7c345ad43631d263
Sherif-Mansour <135798032+Sherif-Mansour@users.noreply.github.com>
2024-03-04 20:44:12 -0700

    Update App page.js links

--- app/page.js
+++ app/page.js
@@ -1,4 +1,4 @@
-import Link from "next/link"
+import Link from "next/link";
 
 export default function Page() {
   return (
@@ -9,7 +9,25 @@ export default function Page() {
         <li>
           <Link href="week-2">Week 2</Link>
         </li>
+        <li>
+          <Link href="week-3">Week 3</Link>
+        </li>
+        <li>
+          <Link href="week-4">Week 4</Link>
+        </li>
+        <li>
+          <Link href="week-5">Week 5</Link>
+        </li>
+        <li>
+          <Link href="week-6">Week 6</Link>
+        </li>
+        <li>
+          <Link href="week-6-new">Week 6 New</Link>
+        </li>
+        <li>
+          <Link href="week-7">Week 7</Link>
+        </li>
       </ul>
     </main>
-  )
+  );
 }
